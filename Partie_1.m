91f3fe4c2853d70730ceefe85f2927ac3d4b152a
Rgnauld <50523003+Rgnauld@users.noreply.github.com>
2020-03-10 12:01:09 +0100

    Update Partie_1.m

--- Partie_1.m
+++ Partie_1.m
@@ -5,7 +5,7 @@
 m = 22740;
 J = 2.47*10^6;
 B = 31;
-epsilon = 0.003;
+xhi = 0.003;
 f_z = 0.10;
 f_theta = 0.278;
 
@@ -16,7 +16,21 @@ L_w = 20;
 I_w = 0.05;
 
 %A) Fct de transfert:
-M = [m;J];
+w_0 = [2*pi*f_z; 2*pi*f_theta];
+M = [m,0;0,J];
+K = w_0.^2.*M; 
+C = 2*xhi*sqrt(K*M);%C = 2*xhi*w_0.*M;
 
-C = 2*epsilon*sqrt(K*M);
+% M*x_ddot + C*x_dot + K*x = f
+a0 = 1;
+a1 = -0.165;
+a2 = -0.335;
+b1 = 0.0455;
+b2 = 0.3;
+w_1 = 2*U*b1/B;
+w_2 = 2*U*b2/B;
+C_omega = a0 + a1*(1i*omega)/(1i*omega + w_1) + a2*(1i*omega)/(1i*omega + w_2);
+q = pi*rho*U^2*B;
+
+
 
