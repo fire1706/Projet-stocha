3a3484b325afd615a2a9a369f08da9f39a4e9501
Rgnauld <50523003+Rgnauld@users.noreply.github.com>
2020-03-17 10:58:27 +0100

    coucou

--- Partie_1.m
+++ Partie_1.m
@@ -2,6 +2,8 @@
 
 %DONNEES:
 %1) Propriétés PONT
+i = 1;
+w = 0:0.1:2;
 m = 22740;
 J = 2.47*10^6;
 B = 31;
@@ -12,7 +14,7 @@ f_theta = 0.278;
 %2) Propriétés VENT
 rho = 1.22;
 L_w = 20;
-U = 100;%U = [0,100];
+U = 10;%U = [0,100];
 I_w = 0.05;
 
 %A) Fct de transfert:
@@ -29,16 +31,25 @@ b1 = 0.0455;
 b2 = 0.3;
 w_1 = 2*U*b1/B;
 w_2 = 2*U*b2/B;
-C_w = C_omega(w,U,B);
-q = pi*rho*U^2*B;
-Grosse_matrice_desesmorts = [B*w^2/(4*U^2) - C_w*1i*w/U , B/(4*U)*1i*w + C_w*(1+B/(4*U)*1i*w) ; -B/4*C_w*1i*w/U , B/4*(B^2*w^2/(32*U^2) - B/(4*U)*1i*w +  C_w*(1+B/(4*U)*1i*w))];
-H = 1./(-w^2*M + 1i*w*C + K - q*Grosse_matrice_desesmorts);
-
+j = 1;
+H = zeros(2,2);
+while i <= length(w)    
+    C_w(i) = C_omega(w(i),U,B);
+    q = pi*rho*U^2*B;
+    Grosse_matrice_desesmorts = [B*w(i)^2/(4*U^2) - C_w*1i*w(i)/U , B/(4*U)*1i*w(i) + C_w*(1+B/(4*U)*1i*w(i)) ; -B/4*C_w*1i*w(i)/U , B/4*(B^2*w(i)^2/(32*U^2) - B/(4*U)*1i*w(i) +  C_w*(1+B/(4*U)*1i*w(i)))];
+    H(i,j) = (-w(i)^2*M + 1i*w(i)*C + K - q*Grosse_matrice_desesmorts).^(-1);
+    j = j + 1;
 % F_se_omega = q*Grosse_matrice_desesmorts*[POINT_interro1;POINT_interro2]; 
 % F_b_omega = 1/4*rho*U*B*[4*pi;pi*B]*W_omega;
 % H = X/fb;
 
 %B) Calcul densité de puissance 
-S_w = S_omega(w);
+    S_w(i) = S_omega(w(i),U);
+    i = i + 1;
+end
+%C)
+M_conj = [pi*rho*B^2/4,0;0,pi*rho*B^4/128];
+C_conj = [0,-q*B/(4*U);0,q*B^2/(16*U)];
+
 
 
--- S_omega.m
+++ S_omega.m
@@ -1,13 +1,8 @@
-function sigma_w = S_omega(w)
-U = 0:1:100;
+function S = S_omega(w,U)
 L_w = 20;
 I_w = 0.05;
-i = 0;
-S = zeros;
-while i <= 100
-    sigma_w = U(i)*I_w;
-    S(i) = sigma_w^2*L_w/(pi*U(i)) *(1+755.2*(w*L_w/(2*pi*U(i)))^2)/(1+283.2*(w*L_w/(2*pi*U(i)))^2)^(11/6);
-end
+sigma_w = U*I_w;
+S = sigma_w^2*L_w/(pi*U) *(1+755.2*(w*L_w/(2*pi*U))^2)/(1+283.2*(w*L_w/(2*pi*U))^2)^(11/6);
 end
 
 
